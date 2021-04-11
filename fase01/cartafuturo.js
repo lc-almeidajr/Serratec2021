5a4c5166179e7552fda80a90e00fa90f302f8aa3
lc-almeidajr <81425611+lc-almeidajr@users.noreply.github.com>
2021-04-10 22:07:30 -0300

    Exercícios Aula 01

---
+++ fase01/cartafuturo.js
@@ -0,0 +1,19 @@
+  console.log("Carta para o Futuro");
+  console.log("Toda grande caminhada começa com o primeiro passo. Começamos a caminhada para uma virada profissional em nossas vidas.");
+  console.log("Quando estiver lendo essa carta é provável que tenha acabado o curso de Lógica de programação.");
+  console.log("Estará entendendo como funciona uma cadeia de algoritmos.");
+  console.log("A partir de agora mais do que entender como funciona, você irá criar. As manhãs sonolentas que eram superadas com ânimo, esperança e café, isso, não vai mudar." , "");
+  console.log("Duas coisas não serão tiradas de você; as lembranças e o conhecimento adquirido. Agora preare-se para fazer acontencer." , "Pense, crie, salve e roda!")
+
+
+
+
+
+
+
+
+
+
+
+
+
